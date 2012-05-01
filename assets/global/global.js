df9e9d199447100d952eb96e993ba41001512f24
unknown <yoom@song.(none)>
2012-05-01 20:56:53 +0800

    first commit

---
+++ assets/global/global.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: yoom
+ * Date: 12-5-1
+ * Time: 下午8:20
+ * To change this template use File | Settings | File Templates.
+ */
+
+alert(123)
---
+++ run.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: yoom
+ * Date: 12-5-1
+ * Time: 下午7:35
+ * To change this template use File | Settings | File Templates.
+ */
+
+var express = require('express'),
+    app = express.createServer();
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+    res.render('home',{rand:Math.random()});
+});
+
+app.listen(8000);
